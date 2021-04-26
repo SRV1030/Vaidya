03d419ee3cb25fcd020e4e54059bb4e53b605617
Sourabh Shah <shahsourabh11@gmail.com>
2021-04-26 20:04:13 +0545

    next changes

--- static/homepage.js
+++ static/homepage.js
@@ -18,28 +18,3 @@ function myFunction() {
         x.className = "topnav";
     }
 }
-
-// $(".nav-item").click(function() {
-//     $(".nav-item").removeClass("active");
-//     $(this).addClass("active");
-//     // localStorage.ClassName = "working";
-// });
-
-// $(document).ready(function() {
-//     SetClass();
-// });
-
-// function SetClass() {
-//     //before assigning class check local storage if it has any value
-//     $("#themecolors li a").addClass(localStorage.ClassName);
-// }
-
-
-// $(".nav-item").on("click", function(e) {
-//     $(".nav-item").removeClass("active");
-//     $(this).addClass("active");
-// });
-
-// $(function() {
-//   $('topnav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
-// });
