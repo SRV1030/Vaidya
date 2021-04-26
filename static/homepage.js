2f41ea1aeeb7cb23283d8263b0e6059846415c48
Sourabh Shah <shahsourabh11@gmail.com>
2021-04-26 18:04:34 +0545

    navbarfis

--- static/homepage.js
+++ static/homepage.js
@@ -1,11 +1,11 @@
 //Carousel
-var counter=1;
-setInterval(function(){
-  document.getElementById('radio' + counter).checked=true;
-  counter++;
-  if(counter>3){
-    counter=1;
-  }
+var counter = 1;
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 3) {
+        counter = 1;
+    }
 }, 5000);
 
 
@@ -13,24 +13,33 @@ setInterval(function(){
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+        x.className += " responsive";
     } else {
-      x.className = "topnav";
+        x.className = "topnav";
     }
-  }
+}
 
-//   $(document).ready(function(){
-//     $('li').click(function() {
-//     $("li.active").removeClass("active");
-//     $(this).addClass('active');
+// $(".nav-item").click(function() {
+//     $(".nav-item").removeClass("active");
+//     $(this).addClass("active");
+//     // localStorage.ClassName = "working";
 // });
 
-  // $(".nav-item").on("click", function(e) {
-  //   $(".nav-item").removeClass("active");
-  //   $(this).addClass("active");
-  //   // e.preventDefault();
-  // });
+// $(document).ready(function() {
+//     SetClass();
+// });
 
-  $(function() {
-    $('topnav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
-  });
+// function SetClass() {
+//     //before assigning class check local storage if it has any value
+//     $("#themecolors li a").addClass(localStorage.ClassName);
+// }
+
+
+// $(".nav-item").on("click", function(e) {
+//     $(".nav-item").removeClass("active");
+//     $(this).addClass("active");
+// });
+
+// $(function() {
+//   $('topnav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+// });
